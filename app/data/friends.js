a3be76a4a74ae45f3b024d3b539fde97aeeeeae5
aryannawim <aryannawim@gmail.com>
2017-02-08 17:30:38 -0600

    Add files via upload

---
+++ app/data/friends.js
@@ -0,0 +1,26 @@
+var friends = [
+
+{
+	name: 'Billy',
+	image: 'http://www.peta.org/wp-content/uploads/2016/02/Goats-maximili-602x403.jpg',
+	scores: [2, 3, 4, 5, 3, 2, 1, 2, 3, 5]
+},
+{
+	name: 'Donald',
+	image: 'http://c1.nrostatic.com/sites/default/files/uploaded/donald-trump-incapable-of-embarrassment-r_0.jpg',
+	scores: [1, 1, 2, 3, 1, 4, 5, 2, 3, 1]
+},
+{
+	name: 'Bernie',
+	image: 'http://images.dailykos.com/images/199343/story_image/BernieHoldingACat.jpg',
+	scores: [4, 5, 4, 2, 3, 1, 3, 5, 3, 4]
+},
+{
+	name: 'Kevin',
+	image: 'http://i.imgur.com/evxHspS.jpg',
+	scores: [4, 4, 4, 5, 5, 5, 2, 5, 5, 5]
+}
+
+];
+
+module.exports = friends;
